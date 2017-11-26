ac670698bc68ea3e747dd13dc52fd6eca647196e
Vlad <vovernak@gmail.com>
2017-11-26 16:15:35 +0300

    Initial Commit

---
+++ HomeWork-Saprykin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HomeWork-Saprykin
+//
+//  Created by Vlad Saprykin on 26.11.2017.
+//  Copyright © 2017 Vlad Saprykin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
